f73ede7cce5f20176470c34b689117071b25e948
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-03-17 18:26:29 +0530

    Update PricingPage.js

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "./Hero";
 import Brokerage from "./Brokerage";
 import OpenAccount from "../OpenAccount";
-
 function PricingPage() {
   return (
     <>
