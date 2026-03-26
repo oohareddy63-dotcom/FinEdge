3030e3b4afbb59b2a09e2a12ab4be8f6e8d0ed91
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-03-26 08:49:47 +0530

    Update PositionsModel.js

--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -1,7 +1,5 @@
 const { model } = require("mongoose");
-
 const { PositionsSchema } = require("../schemas/PositionsSchema");
-
 const PositionsModel = new model("position", PositionsSchema);
 
 module.exports = { PositionsModel };
